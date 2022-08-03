ab68bb578479a480a5434f1dd30782e617d5d761
Rahid Aliyev <rahidaliyev18@gmail.com>
2022-08-03 17:44:14 +0400

    error button added

---
+++ src/pages/button/CustomErrorButton.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@material-ui/core'
+
+export default function CustomErrorButton() {
+  return (
+    <div>
+<Button variant="outlined" color="error">
+  Error
+</Button>
+
+    </div>
+  )
+}
